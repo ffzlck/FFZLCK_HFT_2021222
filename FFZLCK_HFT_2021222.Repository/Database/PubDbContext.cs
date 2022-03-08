e66d568454bbc5cb67bdb63af572bcd489636820
FFZLCK <FFZLCK@stud.uni-obuda.hu>
2022-03-08 14:22:00 +0100

    add DbContext

---
+++ FFZLCK_HFT_2021222.Repository/Database/PubDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFZLCK_HFT_2021222.Repository.Database
+{
+    public class PubDbContext : DbContext
+    {
+        public PubDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseLazyLoadingProxies().UseInMemoryDatabase("pub");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
