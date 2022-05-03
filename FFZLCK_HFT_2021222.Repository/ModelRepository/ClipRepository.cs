fdf9aa31ae9c793be48041b7fcc622ae997796d2
FFZLCK <FFZLCK@stud.uni-obuda.hu>
2022-05-03 15:27:58 +0200

    added model repository classes

---
+++ FFZLCK_HFT_2021222.Repository/ModelRepository/ClipRepository.cs
@@ -0,0 +1,37 @@
+﻿using FFZLCK_HFT_2021222.Models;
+using FFZLCK_HFT_2021222.Repository.Database;
+using FFZLCK_HFT_2021222.Repository.GenericRepository;
+using FFZLCK_HFT_2021222.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFZLCK_HFT_2021222.Repository.ModelRepository
+{
+    public class ClipRepository : Repository<Clip>, IRepository<Clip>
+    {
+        public ClipRepository(MusicDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Clip Read(int id)
+        {
+            return ctx.Clips.FirstOrDefault(x => x.ClipID == id);
+        }
+
+        public override void Update(Clip item)
+        {
+            var old = Read(item.ClipID);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                if (prop.GetAccessors().FirstOrDefault(t => t.IsVirtual) == null)
+                {
+                    prop.SetValue(old, prop.GetValue(item));
+                }
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
