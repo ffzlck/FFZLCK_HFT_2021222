2b7a16de069834dd7f3995b309856a8e19306a1c
FFZLCK <FFZLCK@stud.uni-obuda.hu>
2022-05-03 18:48:28 +0200

    endpoint startup write

--- FFZLCK_HFT_2021222.Endpoint/Startup.cs
+++ FFZLCK_HFT_2021222.Endpoint/Startup.cs
@@ -1,8 +1,16 @@
+using FFZLCK_HFT_2021222.Logic.Interfaces;
+using FFZLCK_HFT_2021222.Logic.Logics;
+using FFZLCK_HFT_2021222.Models;
+using FFZLCK_HFT_2021222.Repository.Database;
+using FFZLCK_HFT_2021222.Repository.Interfaces;
+using FFZLCK_HFT_2021222.Repository.ModelRepository;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +24,23 @@ namespace FFZLCK_HFT_2021222.Endpoint
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<MusicDbContext>();
+
+            services.AddTransient<IRepository<Album>, AlbumRepository>();
+            services.AddTransient<IRepository<Music>, MusicRepository>();
+            services.AddTransient<IRepository<Clip>, ClipRepository>();
+            services.AddTransient<IRepository<Performer>, PerformerRepository>();
+
+            services.AddTransient<IAlbumLogic, AlbumLogic>();
+            services.AddTransient<IMusicLogic, MusicLogic>();
+            services.AddTransient<IClipLogic, ClipLogic>();
+            services.AddTransient<IPerformerLogic, PerformerLogic>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FFZLCK_HFT_2021222.Endpoint", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +49,27 @@ namespace FFZLCK_HFT_2021222.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FFZLCK_HFT_2021222.Endpoint v1"));
             }
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
+
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
