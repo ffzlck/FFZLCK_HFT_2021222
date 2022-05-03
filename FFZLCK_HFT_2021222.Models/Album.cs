20f8170e033b47436055402bee77cf1d445df1ad
FFZLCK <FFZLCK@stud.uni-obuda.hu>
2022-05-03 20:49:31 +0200

    jsonignore added

--- FFZLCK_HFT_2021222.Models/Album.cs
+++ FFZLCK_HFT_2021222.Models/Album.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace FFZLCK_HFT_2021222.Models
@@ -15,6 +16,7 @@ namespace FFZLCK_HFT_2021222.Models
         public int AlbumID { get; set; }
         public string AlbumName { get; set; }
         public int AlbumPopularity { get; set; }
+        [JsonIgnore]
         public virtual ICollection<Music> Musics { get; set; }
         public int PerformerID { get; set; }
         public virtual Performer Performer { get; set; }
--- FFZLCK_HFT_2021222.Models/Performer.cs
+++ FFZLCK_HFT_2021222.Models/Performer.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace FFZLCK_HFT_2021222.Models
@@ -15,8 +16,11 @@ namespace FFZLCK_HFT_2021222.Models
         public int PerformerID { get; set; }
         public string PerformerName { get; set; }
         public string PerformerStyle { get; set; }
+        [JsonIgnore]
         public virtual ICollection<Music> Musics { get; set; }
+        [JsonIgnore]
         public virtual ICollection<Album> Albums { get; set; }
+        [JsonIgnore]
         public virtual ICollection<Clip> Clips { get; set; }
 
         public Performer()
