be159695cd7aa41d20b5960413c58f4a0c29a659
FFZLCK <FFZLCK@stud.uni-obuda.hu>
2022-05-04 13:11:05 +0200

    modify startup

--- FFZLCK_HFT_2021222.Endpoint/Startup.cs
+++ FFZLCK_HFT_2021222.Endpoint/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
@@ -20,6 +21,12 @@ namespace FFZLCK_HFT_2021222.Endpoint
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
